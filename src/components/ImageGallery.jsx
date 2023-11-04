46feb18f14c06b2f91b303f7d41acdd490b35a57
yeasinshakil <shakilrock1998@gmail.com>
2023-11-05 01:06:26 +0600

    responsiveness & logo icon

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -68,11 +68,11 @@ const ImageGallery = () => {
   // console.log(selectedImages);
 
   return (
-    <div className=" min-h-[600px] h-full pb-10 w-[1000px] bg-white mx-auto my-5 rounded-md shadow-md">
+    <div className=" sm:min-h-[600px] h-full pb-6 sm:pb-10 md:w-[600px] xl:w-[1000px] bg-white mx-2 sm:mx-auto my-5 rounded-md shadow-md">
       <div className=" my-5 border-b-2 border-black/50 flex justify-between p-4">
         {/* Heading will change when any item will select */}
         {selectedImages.length > 0 ? (
-          <div className=" flex gap-2 items-center text-xl font-bold">
+          <div className=" flex gap-2 items-center text-sm sm:text-xl font-bold">
             <input
               type="checkbox"
               className=" w-4 h-4"
@@ -101,7 +101,7 @@ const ImageGallery = () => {
 
       <div
         onDragOver={dragOver}
-        className=" grid gap-4 grid-cols-5 items-center mx-4 "
+        className=" grid gap-2 sm:gap-3 xl:gap-4 grid-cols-3 sm:grid-cols-5 items-center sm:mx-4 mx-2 "
       >
         {images.map((image, index) => {
           return (
@@ -111,24 +111,24 @@ const ImageGallery = () => {
               onDragEnd={(e) => dragEnd(e, index)}
               draggable
               key={index}
-              className={` w-full h-full border border-black p-0 rounded-md cursor-pointer shadow-sm relative group transition-all ${
+              className={` w-full h-full sm:border border-black p-0 rounded-lg sm:rounded-md cursor-pointer shadow-sm relative group transition-all ${
                 selectedImages.includes(index)
                   ? " border-2 border-blue-600 "
                   : ""
               }  ${index === 0 ? " col-span-2 row-span-2" : ""}`}
             >
               <div
-                className={` w-full h-full bg-blue-950 absolute top-0 left-0 opacity-0 group-hover:opacity-70 transition-all duration-300 ${
+                className={` w-full h-full bg-blue-950 p-0 rounded-lg sm:rounded-md absolute top-0 left-0 opacity-0 group-hover:opacity-70 transition-all duration-300 ${
                   selectedImages.includes(index) ? " !opacity-30 " : ""
                 }`}
               ></div>
               <img
-                className=" w-full h-full rounded-md bg-black hover:opacity-50"
+                className=" w-full h-full rounded-lg sm:rounded-md bg-black hover:opacity-50"
                 src={image}
                 alt={`Image ${index}`}
               />
               <input
-                className={`absolute top-4 left-4 hidden cursor-pointer group-hover:block w-5 h-5 ${
+                className={`absolute top-2 left-2 sm:top-4 sm:left-4 hidden cursor-pointer group-hover:block sm:w-5 sm:h-5 h-4 w-4 ${
                   selectedImages.includes(index) ? " !block " : ""
                 }`}
                 type="checkbox"
@@ -156,6 +156,12 @@ const ImageGallery = () => {
           <p className=" font-semibold">Add Images</p>
         </div>
       </div>
+      {/* Image upload alert */}
+      {images.length <= 0 && (
+        <p className=" text-center animate-pulse text-red-500 text-lg sm:text-2xl sm:pt-20 pt-10">
+          Please upload images to see the collage
+        </p>
+      )}
     </div>
   );
 };
