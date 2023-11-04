72f3812ac4ab9e1ca4872e69ca56ef9b05330ddf
yeasinshakil <shakilrock1998@gmail.com>
2023-11-04 19:44:19 +0600

    initial commit

---
+++ src/components/ImageGallery.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ImageGallery = () => {
+  return (
+    <div>
+      <p className=" text-5xl">Image Gallery</p>
+    </div>
+  );
+};
+
+export default ImageGallery;
